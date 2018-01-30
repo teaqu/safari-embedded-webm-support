4579f7babb7faa872dbb298c139bc4735612e1ed
C9HDN <c9h0xdn@gmail.com>
2018-01-29 22:40:28 -0500

    Gelbooru & Danbooru support. Larger video player. Closes #3.

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 ![4chan Screenshot](/images/screenshot.png?raw=true')
 
 # Safari Embedded WebM Support
+Plays embedded WebM videos playing in Safari on supported imageboards: 4chan, 8chan, Gelbooru and Danbooru.
 
-Gets embedded WebM videos playing in Safari on supported imageboards: 4chan and 8chan.
-
-This script requires two more safari extensions to run. 
-
-First install [Tampermonkey](https://tampermonkey.net/?browser=safari) and the [VLC Web Plugin](http://www.videolan.org/vlc/download-macosx.html). The VLC Web Plugin can be found by clicking on the dropdown.
-
-Next, [click here](https://github.com/b50/safari-embedded-webm-support/raw/master/webm.user.js) to install this script.
-
-That's it for 8chan however to get this working on 4chan, install [4chan X](https://github.com/ccd0/4chan-x) and disable Image Hover in the settings. 
+## Setup
+1. Install [Tampermonkey](https://tampermonkey.net/?browser=safari)
+2. Install the [VLC Web Plugin](http://www.videolan.org/vlc/download-macosx.html) which can be found by clicking on the dropdown.
+3. [click here](https://github.com/b50/safari-embedded-webm-support/raw/master/webm.user.js) to install this script.
+4. [optional] To get this working on 4chan, install [4chan X](https://github.com/ccd0/4chan-x) and disable Image Hover in the settings. 
--- webm.user.js
+++ webm.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         Safari Embedded WebM Support
 // @namespace    safari-embedded-webm-support
-// @version      1.0.1
+// @version      1.1
 // @description  Uses the VLC webplugin to play embeded webM videos.
 // @author       C9HDN
 // @match        *://boards.4chan.org/*
 // @match        *://8ch.net/*
+// @match        *://gelbooru.com/index.php?page=post&*
+// @match        *://danbooru.donmai.us/posts/*
 // @grant        none
 // @require      http://code.jquery.com/jquery-3.2.1.min.js
 // @require      https://raw.githubusercontent.com/uzairfarooq/arrive/master/minified/arrive.min.js
@@ -15,21 +17,40 @@
 
 (function() {
 
-    function embedVLC(video) {
-
+    function embedVLC(video, src) {
         // 4chan
         if (document.URL.indexOf("//boards.4chan") > -1) {
-            insert4chan(video);
-        } else if (document.URL.indexOf("//8ch") > -1) { // 8chan
-            insert8chan(video);
+            insert4chan(video, src);
+        // Other
+        } else {
+             // Create video
+             var vlc = createVLC(video, src);
+             video.before(vlc);
+
+             // Hide video
+             video.attr('style', 'width: 0; opacity: 0; height: 0;');
         }
     }
 
+    // Create vlc video to insert
+    function createVLC(video, src) {
+        // Get source
+        var vlc = $('<embed loop="' + video.prop("loop") + '">');
+        vlc.attr({
+            type: "application/x-vlc-plugin",
+            pluginspage: "http://www.videolan.org",
+            width: "855",
+            height: "481",
+            target: src,
+        });
+        return vlc;
+    }
+
     // 4chan insert
-    function insert4chan(video) {
+    function insert4chan(video, src) {
 
         // Add vlc
-        var vlc = createVLC(video);
+        var vlc = createVLC(video, src);
         video.parent().before(vlc);
         vlc.attr("style", "margin: 5px 20px");
 
@@ -51,33 +72,18 @@
         });
     }
 
-    // 8chan insert
-    function insert8chan(video) {
-
-        // Create video
-        var vlc = createVLC(video);
-        video.before(vlc);
-
-        // Hide video
-        video.attr('style', 'width: 0; opacity: 0; height: 0;');
-    }
-
-    // Create vlc video to insert
-    function createVLC(video) {
-        var vlc = $('<embed loop="true">');
-        vlc.attr({
-            type: "application/x-vlc-plugin",
-            pluginspage: "http://www.videolan.org",
-            width: "640",
-            height: "360",
-            target: video.attr("src"),
-        });
-        return vlc;
-    }
-
-    // Convert new
+    // Convert html5
     $(document).arrive('video[src$=".webm"]', function() {
-        embedVLC($(this));
+        embedVLC($(this), $(this).attr("src"));
+    });
+    $('video[src$=".webm"]').each(function() {
+        embedVLC($(this), $(this).attr("src"));
+    });
+     $(document).arrive('video source[src$=".webm"]', function() {
+        embedVLC($(this).closest('video'), $(this).attr("src"));
+    });
+    $('video source[src$=".webm"]', $(this).src).each(function() {
+        embedVLC($(this).closest('video'), $(this).attr("src"));
     });
 
 })(this);
